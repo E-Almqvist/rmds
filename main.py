26f9dd4681018eb74038708fc405c369b1acc2bc
E. Almqvist <elalmqvist@gmail.com>
2021-01-16 02:32:44 +0100

    Release v1.0

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ art = """
                 ||     ||
 """
 
-
-print(art)
-
 # reading the auth token
 authfile = open("auth.txt", "r")
 authToken = authfile.readlines()[0]
@@ -40,11 +37,11 @@ async def get_target_server(c):
 
 @c.event
 async def on_ready():
-	print(f"\033[92mLogged in as: {c.user}\033[0m")
+	print(f"\033[92mLogged in as \033[1m{c.user}\033[0m")
 	await get_target_server(c)
 
 async def attack_server(server, c):
-	print(f"Wiping server: \033[1m {server} \033[0m")
+	print(f"Wiping server:  \033[1m{server} \033[0m")
 
 	if( not key_valid("--noask") ):
 		print(f"\nAll of your messages in \033[1m{server}\033[0m will be \033[1m\033[91mDELETED FOREVER\033[0m!\033[0m")
@@ -64,12 +61,13 @@ async def attack_server(server, c):
 
 async def get_user_messages(channel, c):
 	messages = await channel.history(limit=9999).flatten()
+	out = list()
 
 	for m in messages:
-		if( m.author != c.user ):
-			messages.remove(m)
+		if( m.author == c.user ):
+			out.append(m)
 
-	return messages, len(messages)
+	return out, len(out)
 
 async def send_payload(channel, c):
 	user_messages, user_messages_len = await get_user_messages(channel, c)
@@ -79,11 +77,21 @@ async def send_payload(channel, c):
 	for i, m in enumerate(user_messages):
 		try:
 			print(f"[{i+1}/{user_messages_len}]\t \033[1m\033[91mrm\033[0m \t({m.id}) : \"{m.content}\"")
+			await m.delete()
 		except Exception as err:
-			print(f"err: {err}")
+			print(f"Error: {err}")
 			pass
 
 
+def init():
+	print(art)
+	try:
+		print("\033[1mLogging in...\033[0m")
+		c.run(authToken, bot=False)
+	except Exception as err:
+		print("\033[1mFailed to login.\033[0m")
+		print(f"Error: {err}")
 
 
-c.run(authToken, bot=False)
+if __name__ == "__main__":
+	init()
